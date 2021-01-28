50cba02a9f9cb9dbffb19cbf50672030cb5354b3
yushi1999 <kontani1999@gmail.com>
2021-01-29 08:27:33 +0900

    Firestoreを使ってみる

--- firebaseapp/lib/main.dart
+++ firebaseapp/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -6,7 +6,6 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,83 +15,97 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyAuthPage(),
+      home: MyFirestorePage(),
     );
   }
 }
 
-class MyAuthPage extends StatefulWidget {
+class MyFirestorePage extends StatefulWidget {
   @override
-  _MyAuthPageState createState() => _MyAuthPageState();
+  _MyFirestorePageState createState() => _MyFirestorePageState();
 }
 
-class _MyAuthPageState extends State<MyAuthPage> {
-  // 入力されたメールアドレス
-  String newUserEmail = "";
-  // 入力されたパスワード
-  String newUserPassword = "";
-  // 入力されたメールアドレス（ログイン）
-  String loginUserEmail = "";
-  // 入力されたパスワード（ログイン）
-  String loginUserPassword = "";
-  // 登録・ログインに関する情報を表示
-  String infoText = "";
+class _MyFirestorePageState extends State<MyFirestorePage> {
+  List<DocumentSnapshot> documentList = [];
+  String orderDocumentInfo = "";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Container(
-          padding: EdgeInsets.all(32),
-          child: Column(
-            children: <Widget>[
-              TextFormField(/* --- 省略 --- */),
-              TextFormField(/* --- 省略 --- */),
-              RaisedButton(/* --- 省略 --- */),
-              Container(height: 32),
-              TextFormField(
-                decoration: InputDecoration(labelText: "メールアドレス"),
-                onChanged: (String value) {
-                  setState(() {
-                    loginUserEmail = value;
-                  });
-                },
-              ),
-              TextFormField(
-                decoration: InputDecoration(labelText: "パスワード"),
-                obscureText: true,
-                onChanged: (String value) {
-                  setState(() {
-                    loginUserPassword = value;
-                  });
-                },
-              ),
-              RaisedButton(
+        child: Column(
+          children: <Widget>[
+            RaisedButton(
+              child: Text('コレクション＋ドキュメント作成'),
+              onPressed: () async {
+                // ドキュメント作成
+                await Firestore.instance
+                    .collection('users') // コレクションID
+                    .document('id_abc') // ドキュメントID
+                    .setData({'name': '鈴木', 'age': 40}); // データ
+              },
+            ),
+            RaisedButton(
+              child: Text('サブコレクション＋ドキュメント作成'),
+              onPressed: () async {
+                // サブコレクション内にドキュメント作成
+                await Firestore.instance
+                    .collection('users') // コレクションID
+                    .document('id_abc') // ドキュメントID << usersコレクション内のドキュメント
+                    .collection('orders') // サブコレクションID
+                    .document('id_123') // ドキュメントID << サブコレクション内のドキュメント
+                    .setData({'price': 600, 'date': '9/13'}); // データ
+              },
+            ),
+            RaisedButton(
+              child: Text('ドキュメント一覧取得'),
+              onPressed: () async {
+                //コレクション内のドキュメント一覧を取得
+                final snapshot =
+                    await Firestore.instance.collection('users').getDocuments();
+                //取得したドキュメント一覧をUIに反映
+                setState(() {
+                  documentList = snapshot.documents;
+                });
+              },
+            ),
+            //コレクション内のドキュメント一覧を表示
+            Column(
+              children: documentList.map((document) {
+                return ListTile(
+                  title: Text("${document["name"]}さん"),
+                  subtitle: Text("${document["age"]}歳"),
+                );
+              }).toList(),
+            ),
+            RaisedButton(
+              child: Text('ドキュメントを指定して取得'),
+              onPressed: () async {
+                //コレクションIDとドキュメントIDを指定して取得
+                final document = await Firestore.instance
+                    .collection('users')
+                    .document('id_abc')
+                    .collection('orders')
+                    .document('id_123')
+                    .get();
+                //取得したドキュメントの情報をUIに反映
+                setState(() {
+                  orderDocumentInfo =
+                      '${document['date']}${document['price']}円';
+                });
+              },
+            ),
+            //ドキュメントの情報を表示
+            ListTile(title: Text(orderDocumentInfo)),
+            RaisedButton(
+                child: Text('ドキュメント更新'),
                 onPressed: () async {
-                  try {
-                    // メール/パスワードでログイン
-                    final FirebaseAuth auth = FirebaseAuth.instance;
-                    final AuthResult result =
-                        await auth.signInWithEmailAndPassword(
-                      email: loginUserEmail,
-                      password: loginUserPassword,
-                    );
-                    // ログインに成功した場合
-                    final FirebaseUser user = result.user;
-                    setState(() {
-                      infoText = "ログインOK：${user.email}";
-                    });
-                  } catch (e) {
-                    // ログインに失敗した場合
-                    setState(() {
-                      infoText = "ログインNG：${e.message}";
-                    });
-                  }
-                },
-                child: Text("ログイン"),
-              ),
-              Text(infoText),
-            ],
-          ),
+                  //ドキュメント更新
+                  await Firestore.instance
+                      .collection('users')
+                      .document("id_abc")
+                      .updateData({'age': 41});
+                })
+          ],
         ),
       ),
     );
