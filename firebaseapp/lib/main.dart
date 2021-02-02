94c844c99a830bc28ed99cd24b3277a897cf871e
yushi1999 <kontani1999@gmail.com>
2021-02-03 01:35:55 +0900

    Authentificationでログイン

--- firebaseapp/lib/main.dart
+++ firebaseapp/lib/main.dart
@@ -1,111 +1,210 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 void main() {
-  runApp(MyApp());
+  // 最初に表示するWidget
+  runApp(ChatApp());
 }
 
-class MyApp extends StatelessWidget {
+class ChatApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      // 右上に表示される"debug"ラベルを消す
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      // アプリ名
+      title: 'ChatApp',
       theme: ThemeData(
+        // テーマカラー
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyFirestorePage(),
+      // ログイン画面を表示
+      home: LoginPage(),
     );
   }
 }
 
-class MyFirestorePage extends StatefulWidget {
+// ログイン画面用Widget
+class LoginPage extends StatefulWidget {
   @override
-  _MyFirestorePageState createState() => _MyFirestorePageState();
+  _LoginPageState createState() => _LoginPageState();
 }
 
-class _MyFirestorePageState extends State<MyFirestorePage> {
-  List<DocumentSnapshot> documentList = [];
-  String orderDocumentInfo = "";
+class _LoginPageState extends State<LoginPage> {
+  // メッセージ表示用
+  String infoText = '';
+  // 入力したメールアドレス・パスワード
+  String email = '';
+  String password = '';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          children: <Widget>[
-            RaisedButton(
-              child: Text('コレクション＋ドキュメント作成'),
-              onPressed: () async {
-                // ドキュメント作成
-                await Firestore.instance
-                    .collection('users') // コレクションID
-                    .document('id_abc') // ドキュメントID
-                    .setData({'name': '鈴木', 'age': 40}); // データ
-              },
-            ),
-            RaisedButton(
-              child: Text('サブコレクション＋ドキュメント作成'),
-              onPressed: () async {
-                // サブコレクション内にドキュメント作成
-                await Firestore.instance
-                    .collection('users') // コレクションID
-                    .document('id_abc') // ドキュメントID << usersコレクション内のドキュメント
-                    .collection('orders') // サブコレクションID
-                    .document('id_123') // ドキュメントID << サブコレクション内のドキュメント
-                    .setData({'price': 600, 'date': '9/13'}); // データ
-              },
-            ),
-            RaisedButton(
-              child: Text('ドキュメント一覧取得'),
-              onPressed: () async {
-                //コレクション内のドキュメント一覧を取得
-                final snapshot =
-                    await Firestore.instance.collection('users').getDocuments();
-                //取得したドキュメント一覧をUIに反映
-                setState(() {
-                  documentList = snapshot.documents;
-                });
-              },
-            ),
-            //コレクション内のドキュメント一覧を表示
-            Column(
-              children: documentList.map((document) {
-                return ListTile(
-                  title: Text("${document["name"]}さん"),
-                  subtitle: Text("${document["age"]}歳"),
-                );
-              }).toList(),
-            ),
-            RaisedButton(
-              child: Text('ドキュメントを指定して取得'),
-              onPressed: () async {
-                //コレクションIDとドキュメントIDを指定して取得
-                final document = await Firestore.instance
-                    .collection('users')
-                    .document('id_abc')
-                    .collection('orders')
-                    .document('id_123')
-                    .get();
-                //取得したドキュメントの情報をUIに反映
-                setState(() {
-                  orderDocumentInfo =
-                      '${document['date']}${document['price']}円';
-                });
-              },
-            ),
-            //ドキュメントの情報を表示
-            ListTile(title: Text(orderDocumentInfo)),
-            RaisedButton(
-                child: Text('ドキュメント更新'),
-                onPressed: () async {
-                  //ドキュメント更新
-                  await Firestore.instance
-                      .collection('users')
-                      .document("id_abc")
-                      .updateData({'age': 41});
-                })
-          ],
+        child: Container(
+          padding: EdgeInsets.all(24),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              // メールアドレス入力
+              TextFormField(
+                decoration: InputDecoration(labelText: 'メールアドレス'),
+                onChanged: (String value) {
+                  setState(() {
+                    email = value;
+                  });
+                },
+              ),
+              // パスワード入力
+              TextFormField(
+                decoration: InputDecoration(labelText: 'パスワード'),
+                obscureText: true,
+                onChanged: (String value) {
+                  setState(() {
+                    password = value;
+                  });
+                },
+              ),
+              Container(
+                padding: EdgeInsets.all(8),
+                // メッセージ表示
+                child: Text(infoText),
+              ),
+              Container(
+                width: double.infinity,
+                // ユーザー登録ボタン
+                child: RaisedButton(
+                  color: Colors.blue,
+                  textColor: Colors.white,
+                  child: Text('ユーザー登録'),
+                  onPressed: () async {
+                    try {
+                      // メール/パスワードでユーザー登録
+                      final FirebaseAuth auth = FirebaseAuth.instance;
+                      final AuthResult result =
+                          await auth.createUserWithEmailAndPassword(
+                        email: email,
+                        password: password,
+                      );
+                      final FirebaseUser user = result.user;
+                      // ユーザー登録に成功した場合
+                      // チャット画面に遷移＋ログイン画面を破棄
+                      await Navigator.of(context).pushReplacement(
+                        MaterialPageRoute(builder: (context) {
+                          // ユーザー情報を渡す
+                          return ChatPage(user);
+                        }),
+                      );
+                    } catch (e) {
+                      // ユーザー登録に失敗した場合
+                      setState(() {
+                        infoText = "登録に失敗しました：${e.message}";
+                      });
+                    }
+                  },
+                ),
+              ),
+              Container(
+                width: double.infinity,
+                //ログイン登録ボタン
+                child: OutlineButton(
+                    textColor: Colors.blue,
+                    child: Text("ログイン"),
+                    onPressed: () async {
+                      try {
+                        // メール/パスワードでログイン
+                        final FirebaseAuth auth = FirebaseAuth.instance;
+                        final AuthResult result =
+                            await auth.signInWithEmailAndPassword(
+                          email: email,
+                          password: password,
+                        );
+                        final FirebaseUser user = result.user;
+                        // ログインに成功した場合
+                        // チャット画面に遷移＋ログイン画面を破棄
+                        await Navigator.of(context).pushReplacement(
+                          MaterialPageRoute(builder: (context) {
+                            return ChatPage(user);
+                          }),
+                        );
+                      } catch (e) {
+                        // ログインに失敗した場合
+                        setState(() {
+                          infoText = "ログインに失敗しました：${e.message}";
+                        });
+                      }
+                    }),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+//チャット画面用Widget
+class ChatPage extends StatelessWidget {
+  // 引数からユーザー情報を受け取れるようにする
+  ChatPage(this.user);
+  //ユーザー情報
+  final FirebaseUser user;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("チャット"),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.close),
+            onPressed: () async {
+              // 内部で保持しているログイン情報等が初期化される
+              // （現時点ではログアウト時はこの処理を呼び出せばOKと、思うぐらいで大丈夫です）
+              await FirebaseAuth.instance.signOut();
+              //ログイン画面に遷移＋チャット画面を破棄
+              await Navigator.of(context)
+                  .pushReplacement(MaterialPageRoute(builder: (context) {
+                return LoginPage();
+              }));
+            },
+          )
+        ],
+      ),
+      body: Center(
+        //ユーザー情報を表示
+        child: Text("ログイン情報:${user.email}"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () async {
+          // 投稿画面に遷移
+          await Navigator.of(context).push(
+            MaterialPageRoute(builder: (context) {
+              return AddPostPage();
+            }),
+          );
+        },
+      ),
+    );
+  }
+}
+
+// 投稿画面用Widget
+class AddPostPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('チャット投稿'),
+      ),
+      body: Center(
+        child: RaisedButton(
+          child: Text('戻る'),
+          onPressed: () {
+            // 1つ前の画面に戻る
+            Navigator.of(context).pop();
+          },
         ),
       ),
     );
